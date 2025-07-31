57368557b943322f8b1a23b340bba1eb4a03ac98
SHIVANI PRAJAPATI <shivaniprajapati10jan@gmail.com>
2025-07-31 11:19:59 +0530

    back to up button Enhance style

--- script.js
+++ script.js
@@ -326,8 +326,11 @@ window.onscroll = function () {
   const btn = document.getElementById("scrollBtn");
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     btn.classList.add("show");
+    // added bounce animation 
+    btn.classList.add("bounce");
   } else {
     btn.classList.remove("show");
+    btn.classList.remove("bounce");
   }
 };
 
