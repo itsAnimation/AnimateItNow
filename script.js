af905838303b46d9ffb3717faf53fba56cc6bd2c
mahimakathpal <indukathpal@gmail.com>
2025-08-26 11:29:11 +0530

    WIP: save current changes before rebase

--- script.js
+++ script.js
@@ -357,3 +357,10 @@ function scrollToTop() {
 
 
 
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  navLinks.classList.toggle('active');
+});
